30b3edd0b8eb3d421ccee3676b08c47093d404c4
HarrisAlex <alexharris7639@gmail.com>
2022-02-15 12:48:56 -0500

    Initial

---
+++ scripts/contact_form.php
@@ -0,0 +1,44 @@
+<?php
+    $firstName = "";
+    $lastName = "";
+    $companyName = "";
+    $problemWithProduct = "";
+    
+   if ($_SERVER["REQUEST_METHOD"] == "POST")
+   {
+       $firstName = format_data($_POST["firstName"]);
+       $lastName = format_data($_POST["lastName"]);
+       $companyName = format_data($_POST["companyName"]);
+       $problemWithProduct = format_data($_POST["contactReason"]);
+   }
+
+   function format_data($data)
+   {
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+   }
+
+   //Connection
+   $connection = mysqli_connect("minervacg.com", "minekpbj_dbwriter", "qdPKWQ++JT7YTuF2#5>}", "minekpbj_contactRequests");
+
+   if (!$connection)
+   {
+       die("Could not connect: " . mysqli_connect_error());
+   }
+
+   $sql = "INSERT INTO ContactSubmissions (firstName, lastName, companyName, contactReason)
+    VALUES ($firstName, $lastName, $companyName, $problemWithProduct)";
+
+    if (mysqli_query($connection, $sql))
+    {
+        echo ("New Record Created!");
+    }
+    else
+    {
+        echo ("Error: " . $sql . "<br>" . $mysqli_error($connection));
+    }
+
+    $mysqli_close($connection);
+?>
